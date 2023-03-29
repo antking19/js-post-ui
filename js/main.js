4b2686356e253f9b8cf6add314f4582b9c7973d3
cuongtk <cuong.truongkien@gmail.com>
2023-03-29 23:17:06 +0700

    create postApi JS file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 import axiosClient from './api/axiosClient';
+import postApi from './api/postApi';
 
 async function main() {
-    const response = await axiosClient.get('/posts');
-    console.log(response);
+    // const response = await axiosClient.get('/posts');
+    // console.log(response);
+
+    const queryParams = {
+        _page: 1,
+        _limit: 5,
+    };
+
+    const respones = await postApi.getAll(queryParams);
+    console.log(respones);
 }
 
 main();
